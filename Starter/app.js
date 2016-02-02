1aecaa751aa533a358677b5624ee64a56df576f0
Sean McGrath <Sean McGrath>
2016-02-02 20:02:05 +0000

    Initial Commit.

---
+++ Starter/app.js
@@ -0,0 +1,36 @@
+//module
+var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
+
+//routes
+weatherApp.config(function($routeProvider){
+    $routeProvider
+    
+     .when('/', {
+        templateUrl: 'pages/home.htm',
+        controller: 'homeController'
+    })
+    
+    .when('/forecast', {
+        templateUrl: 'pages/forecast.htm',
+        controller: 'forecastController'
+    })
+});
+
+//services
+weatherApp.service('cityService', function(){
+   this.city = "New York, NY" 
+});
+//controllers
+weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService){
+    $scope.city = cityService.city;
+    
+    $scope.$watch('city', function(){
+       cityService.city = $scope.city; 
+    });
+    
+}]);
+
+weatherApp.controller('forecastController', ['$scope', 'cityService', function($scop, cityService){
+    $scope.city = cityService.city;
+}]);
+
